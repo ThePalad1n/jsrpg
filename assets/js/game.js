646b66240d2c71f541130840471bf02a4b536d64
ThePalad1n <evangreen91@gmail.com>
2022-07-19 09:22:34 -0400

    init cmmt

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+var characters = {
+    "MC": {
+        name: "MC",
+        health: 100,
+        attack: 10,
+        defense: 5,
+        imageUrl: ""
+    },
+    "Spider": {
+        name: "Spider",
+        health: 50,
+        attack: 8,
+        defense: 5,
+        imageUrl: ""
+    }
+}
